85086212971b5b1e5373df05244d3e739f089c8e
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-05-21 20:04:05 +0800

    .

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::post('/register', function () {
-    return "Hello thank you";
-});
+Route::post('/register', [UserController::class,'register']);
