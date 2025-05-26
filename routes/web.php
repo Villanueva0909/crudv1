024158a64c6e611c10142822024bf7a03e8cb2dd
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-05-26 18:48:55 +0800

    .

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,6 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::post('/register', [UserController::class,'register']);
+Route::post('/register', [UserController::class, 'register']);
+
+Route::get('/products',[ProductController::class, 'index'])->name( name: 'products.index');
